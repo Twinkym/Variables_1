f7fe2234bcee3eea937d6b6c54bef2700cb06985
david de la puente enriquez <Twinkymen@hotmail.com>
2022-02-10 16:11:28 +0100

    Primero de Variables.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 16;
+
+        System.out.println("El primer numero es: " + num1);
+        System.out.println("El segundo numero es: " + num2);
+
+        System.out.println("La Suma es: " + (num1 + num2));
+        System.out.println("La Resta es: " + (num1 - num2));
+        System.out.println("El producto de la Multiplicación es: " + (num1 * num2));
+        System.out.println("El resultado de la Division es: " + (num1 / num2));
+        System.out.println("Todas las operaciones realizadas!!!!!!!");
+    }
+}
